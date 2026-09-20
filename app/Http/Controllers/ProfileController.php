5c8364517d6e360382ecb1cab20cb36b900fee6d
semailk <sem.alik@mail.ru>
2026-09-20 14:08:00 +0400

    20 09 2026 Установить библиотеку Pricing for Backpack  и настроить админку .            Где будет вывод категорий, пользователей, продуктов.
               Будет также редактирование этих сущностей через админку.
               Самая сложная задача для тех кто хочет получить плюс оценку к курсовой (сделай Tree витвления категория) то есть drag and drop где мы будем перемещать саб категорию под её главную категорию, делаю иерархия категорий.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,7 +16,22 @@ class ProfileController extends Controller
 
     public function update(Request $request)
     {
-       $path = $request->file('avatar')->store('avatars', 'public');
+        $user = auth()->user();
+       $validated = $request->validate([
+            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
+            'name' => 'required|string|max:255|min:3',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+        ]);
 
+        if (isset($validated['avatar'])) {
+            $path = $request->file('avatar')->store('avatars', 'public');
+            $user->avatar = $path;
+        }
+
+        $user->email = $validated['email'];
+        $user->name = $validated['name'];
+        $user->save();
+
+        return redirect()->back()->with('success', 'Profile updated successfully');
     }
 }
